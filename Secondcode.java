73f83ae77b0a60e700eb2e2e64e70483bcabe3ab
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-08-31 07:56:36 +0530

    Add files via upload

---
+++ Secondcode.java
@@ -0,0 +1,13 @@
+public class Secondcode {
+    public static void main(String args[])
+    {
+        int x =18;
+
+        if(x>10)
+        System.out.println("Hello");
+        else
+        System.out.println("Bye");
+        
+        
+    }
+}
