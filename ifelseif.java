244c97e0ce19f8398fd23935f3fd0476dd01762c
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-08-31 08:05:04 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,17 @@
+public class ifelseif {
+    public static void main(String args[])
+    {
+        int x = 8;
+        int y = 6;
+        int z = 9;
+// finding the greatest of the numbers
+       if(x>y && x>z)
+       System.out.println(x);
+       else if (y>x &&y>z)
+       System.out.println(y);
+       else 
+       System.out.println(z);
+    
+
+    }
+}
