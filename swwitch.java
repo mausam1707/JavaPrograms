df8de4539a0e0115c428983ab9171f9444792626
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-08-31 08:32:24 +0530

    Add files via upload

---
+++ swwitch.java
@@ -0,0 +1,27 @@
+public class swwitch 
+{
+    public static void main(String args[]) 
+    {
+    int n=99;
+    switch (n) {
+        case 1:
+            System.out.println("Monday");
+            break;
+        case 2:
+            System.out.println("Tuesday");
+            break;
+        case 3:
+            System.out.println("Wednesday");
+            break;
+        case 4:
+            System.out.println("Thursday");
+            break;
+        case 5:
+            System.out.println("Friday");
+        break;
+    
+        default:
+            System.out.println("Weekend");
+    }
+}
+}
