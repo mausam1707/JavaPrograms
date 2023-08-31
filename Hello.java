4cdf9b3090c42a56e762dc8b41d4e1f72994fb4c
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-08-31 07:20:27 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello
+{
+    public static void main (String args[])
+    {
+       System.out.println("Hello World");
+       System.out.print("I'm going to be a best programmer");
+    }
+}
