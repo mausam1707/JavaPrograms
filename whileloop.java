9ee5e935bf01d02ed7abdb5afbda3cedeb878cd9
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-09-01 07:21:14 +0530

    Add files via upload
    Extended while loop

--- whileloop.java
+++ whileloop.java
@@ -5,6 +5,11 @@ public class whileloop {
         while(i<10)
         {
             System.out.println("Hi " + i);
+            int j=1;
+            while(j<4){
+                System.out.println("Hello " + j);
+                j++;
+            }
             i++;
         }
     }
