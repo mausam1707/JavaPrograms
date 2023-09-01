e318b703c8641cc24eca345a7fe8c88feb8900fa
mausam1707 <143291899+mausam1707@users.noreply.github.com>
2023-09-01 07:39:37 +0530

    Add files via upload

---
+++ dowhile.java
@@ -0,0 +1,13 @@
+public class dowhile {
+    public static void main(String args[])
+    {
+        int i=3;{
+        do
+        {
+            System.out.println("Hi "+i);
+            i++;
+        }while(i<=6);
+           
+        }
+    }
+}
